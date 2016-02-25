2b8024be2552c0622df7143d50bb885b5505520e
Jean-Baptiste FUSS <fuss.jeanbaptiste@gmail.com>
2016-02-25 13:58:54 +0100

    Rules + Card

--- src/main/java/com/fuscho/operation/Rule.java
+++ src/main/java/com/fuscho/operation/Rule.java
@@ -26,36 +26,71 @@ public static class Rule {
                 -> oui tu joues ce que tu veux
                 ->non ton partenaire est maitre
                     -> oui tu joues ce que tu veux
-        ->non tu as de l’atout
-            -> oui tu joues de l’atout
-            -> non tu joues ce que tu veux
+                    ->non tu as de l’atout
+                        -> oui tu joues de l’atout
+                            -> si carte maitre est de l'atout
+                                -> oui si tu as au dessus
+                                    -> oui tu pose un atout au dessus
+                                    -> non tu joues ce que tu veux
+                                -> non tu joues ce que tu veux
+                        -> non tu joues ce que tu veux
      */
 
-    public static List<Card> getPossibleMoves(List<Card> playerHand, SuitCard askedSuit, SuitCard trumpSuit, Card masterCard, Boolean partnerIsMaster){
+    public static List<Card> getPossibleMoves(List<Card> playerHand, SuitCard askedSuit, SuitCard trumpSuit, Card masterCard, Boolean partnerIsMaster) {
 
         List<Card> possibleMoves = playerHand.stream().filter(card -> card.getSuit() == askedSuit).collect(Collectors.toList());
 
         //Player has the asked suit
-        if(possibleMoves > O){
+        if (possibleMoves.size() > 0) {
             //asked suit is trump suit
-            if(askedSuit==trumpSuit){
-                //Player has stronger card than asked
-               /* possibleMoves = possibleMoves.
+            if (askedSuit == trumpSuit) {
+                //Player has stronger card than master card
+                List<Card> possibleMovesHigher = possibleMoves.
                         stream()
-                        .filter(card -> card.getValue() > masterCard.)
-                        .collect(Collectors.toList());*/
+                        .filter(card -> card.getCardOrder(trumpSuit) > masterCard.getCardOrder(trumpSuit))
+                        .collect(Collectors.toList());
+
+                if (possibleMovesHigher.size() > 0) {
+                    possibleMoves = possibleMovesHigher;
+                }
+                //Player hasn't stronger card than asked => DO NOTHING (possibleMoves doesn't change)
 
             }
-            //asked suit isn't trump suit => DO NOTHING JUST RETURN possibleMoves
+            //asked suit isn't trump suit => DO NOTHING (possibleMoves doesn't change)
 
+        } else {
+            //Player hasn't the asked suit
+            //asked suit is trump suit
+            if (askedSuit == trumpSuit) {
+                //Player can play all cards
+                possibleMoves = playerHand;
+            }else{
+                //Player's partner is master
+                if(partnerIsMaster){
+                    //Player can play all cards
+                    possibleMoves = playerHand;
+                }else{
 
+                    if(masterCard.getSuit() == trumpSuit){
+                        //Player has stronger card than masterCard
+                        List<Card> possibleMovesHigher = possibleMoves.
+                                stream()
+                                .filter(card -> card.getCardOrder(trumpSuit) > masterCard.getCardOrder(trumpSuit))
+                                .collect(Collectors.toList());
 
-
-        }else{
-
+                        if (possibleMovesHigher.size() > 0) {
+                            possibleMoves = possibleMovesHigher;
+                        }
+                        //Player hasn't stronger card than asked => Play what you want
+                        possibleMoves = playerHand;
+                    }else{
+                        //Play what you want
+                        possibleMoves = playerHand;
+                    }
+                }
+            }
         }
 
-
-
+        return possibleMoves;
     }
 }
