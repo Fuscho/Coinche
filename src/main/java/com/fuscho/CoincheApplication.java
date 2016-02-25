7966fb37dcbfbc849439c3b4a1d6fce09f6ddf1a
Martin Choraine <martin.choraine@gmail.com>
2016-02-25 08:34:25 +0100

    init git project

---
+++ src/main/java/com/fuscho/CoincheApplication.java
@@ -0,0 +1,12 @@
+package com.fuscho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoincheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoincheApplication.class, args);
+	}
+}
