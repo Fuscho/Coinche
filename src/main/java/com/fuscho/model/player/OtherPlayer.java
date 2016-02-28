37515fbdfa5fdff4308c3071e9de49b84c42c382
Jean-Baptiste FUSS <fuss.jeanbaptiste@gmail.com>
2016-02-28 17:42:03 +0100

    Controllers REST

--- src/main/java/com/fuscho/model/player/OtherPlayer.java
+++ src/main/java/com/fuscho/model/player/OtherPlayer.java
@@ -1,8 +1,18 @@
 package com.fuscho.model.player;
 
+import com.fuscho.model.card.Card;
+import lombok.Data;
+
+import java.util.HashMap;
+
 /**
  * Créer par mchoraine le 25/02/2016.
  */
+@Data
 public class OtherPlayer{
+
+    public HashMap<Integer,Card> possibleMoves = new HashMap<Integer, Card>();
+
+
 
 }
--- src/main/java/com/fuscho/rest/CardController.java
+++ src/main/java/com/fuscho/rest/CardController.java
@@ -3,18 +3,12 @@ package com.fuscho.rest;
 import com.fuscho.model.card.Card;
 import com.fuscho.model.card.SuitCard;
 import com.fuscho.model.card.ValueCard;
-import com.fuscho.model.game.ContractPoint;
-import com.fuscho.model.game.ContractRound;
 import com.fuscho.model.game.Game;
 import com.fuscho.model.game.RoundGame;
-import com.fuscho.model.player.HumanPlayer;
-import com.fuscho.model.player.IAPlayer;
 import com.fuscho.model.player.Player;
 import com.fuscho.operation.Rule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 import java.util.*;
 
 @RestController
@@ -22,38 +16,6 @@ import java.util.*;
 @Slf4j
 public class CardController {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/init")
-    public Map initGame() {
-        Game game = new Game();
-        Player player2 = new IAPlayer("IA1");
-        Player player3 = new IAPlayer("IA2");
-        Player player4 = new IAPlayer("IA3");
-        game.addPlayer(new HumanPlayer("Human"));
-        game.addPlayer(player2);
-        game.addPlayer(player3);
-        game.addPlayer(player4);
-        game.launchGame();
-        game.startRound();
-        Player humanPlayer = game.getPlayers().get(0);
-        Map<String, List<Card>> result = new HashMap<>();
-        result.put("cards", humanPlayer.getCards());
-        result.put("playableCards", Rule.getPossibleMoves(humanPlayer.getCards(), null, null, null, null));
-        return result;
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/bid")
-    public Map bidRound(@RequestBody Map bid) {
-        Player player = Game.getInstance().getPlayers().get(0);
-        RoundGame currentRound = Game.getInstance().getCurrentRound();
-        currentRound.playerBid(player, ContractPoint.fromValue(Integer.parseInt(String.valueOf(bid.get("value")))), SuitCard.valueOf(String.valueOf(bid.get("suit"))));
-        currentRound.startTurn(player);
-        Map<String, Object> result = new HashMap<>();
-        result.put("finishBidding", true);
-        result.put("contractPoint", currentRound.getContractRound().getAskedPoint());
-        result.put("contractSuit", currentRound.getContractRound().getTrumpSuit());
-        result.put("contractBidder", currentRound.getContractRound().getBidder().getName());
-        return result;
-    }
 
     @RequestMapping(method = RequestMethod.POST, value = "/play", produces = "application/json")
     public Map playCard(@RequestBody Map card) {
