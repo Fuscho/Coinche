68dc82f60c7af98084f4140b1d08f776347a03c1
unknown <a614808@FR-CNU1230G19.ww930.my-it-solutions.net>
2016-03-03 16:26:08 +0100

    Mouvement possibles par rapport à la carte joué

---
+++ src/main/java/com/fuscho/ia/MoveToPlay.java
@@ -0,0 +1,14 @@
+package com.fuscho.ia;
+
+/**
+ * Created by a614808 on 03/03/2016.
+ */
+public class MoveToPlay {
+
+    //TODO
+    //CALCUL MIN MAX
+        //Valeur d'un pli
+            //en fonction des cartes tombé
+                // et pondération suivant des cas :
+                    //Ex sous coupé pondération de -10...
+}
--- src/main/java/com/fuscho/ia/PossibleMoves.java
+++ src/main/java/com/fuscho/ia/PossibleMoves.java
@@ -2,6 +2,8 @@ package com.fuscho.ia;
 
 import com.fuscho.model.card.Card;
 import com.fuscho.model.card.factory.CardFactory;
+import com.fuscho.model.game.Game;
+import com.fuscho.model.game.TurnRound;
 import com.fuscho.model.player.IAPlayer;
 import com.fuscho.model.player.OtherPlayer;
 import com.fuscho.model.player.Player;
@@ -46,8 +48,89 @@ public class PossibleMoves {
         return possibleMoves;
     }
 
+    /**
+     * UpdatePossibleMove
+     * Update possible move of otherPlayer after player play Card
+     * @param turnRound
+     * @param playerPlayCard
+     * @param iaPlayer
+     * @param cardPlay
+     * @param otherPlayer
+     */
+    public static void updatePossibleMove(TurnRound turnRound, Player playerPlayCard,Player iaPlayer, Card cardPlay, OtherPlayer otherPlayer){
+        //Update ponderation of possible move of the card play to 0
+        updatePonderation(otherPlayer.possibleMoves,0.0,cardPlay);
+        List<Card> cardPack = CardFactory.createCardPack();
+        if(playerPlayCard==otherPlayer.getPlayer()){
+            //OTHERPLAYER IS PLayerPlayCard
 
-    //TODO UPDATE WHEN CARD IS PLAY (PARAMS cardPlay, GAME)
-    // => foreach otherPlayer (IA) update possible move of player play card
-    //CUT PISS ...
+            //Card play is suit asked
+            if(cardPlay.getSuit()==turnRound.getSuitAsked()){
+                //Suit asked is trump suit
+                if(turnRound.getSuitAsked()==turnRound.getTrumpSuit()){
+                    //Card play is less than masterCard
+                    if(cardPlay.getCardOrder(turnRound.getTrumpSuit())<turnRound.getMasterCard().getCardOrder(turnRound.getTrumpSuit())){
+                        //Update ponderation to 0 for all card > cardPlay
+                        List<Card> cardPackFiltered = cardPack.stream().filter(
+                            card -> card.getSuit()==turnRound.getTrumpSuit()
+                            &&
+                            card.getCardOrder(turnRound.getTrumpSuit())>cardPlay.getCardOrder(turnRound.getTrumpSuit())
+                        ).collect(Collectors.toList());
+
+                        for(Card card : cardPackFiltered){
+                            updatePonderation(otherPlayer.getPossibleMoves(),0.0,cardPlay);
+                        }
+                    }
+                }
+            }else{
+                //Update ponderation of possible move of the cards suit to 0
+                List<Card> cardPackFiltered = cardPack.stream().filter(
+                        card -> card.getSuit()==turnRound.getSuitAsked()
+                ).collect(Collectors.toList());
+
+                for(Card card : cardPackFiltered) {
+                    updatePonderation(otherPlayer.getPossibleMoves(), 0.0, cardPlay);
+                }
+                //SUit asked isn't trump
+                if(turnRound.getSuitAsked()!= turnRound.getTrumpSuit()){
+                    //Partner not master and player doesn't cut
+                    if(!turnRound.isPartenaireMaster(iaPlayer) && cardPlay.getSuit()!=turnRound.getTrumpSuit()){
+                        //masterCard isn't trump suit => player doesn't have trump
+                        if(turnRound.getMasterCard().getSuit()!=turnRound.getTrumpSuit()){
+                            //Update ponderation of possible move of trump cards to 0
+                            cardPackFiltered = cardPack.stream().filter(
+                                    card -> card.getSuit()==turnRound.getTrumpSuit()
+                            ).collect(Collectors.toList());
+
+                            for(Card card : cardPackFiltered){
+                                updatePonderation(otherPlayer.getPossibleMoves(),0.0,cardPlay);
+                            }
+                        }else{
+                            //Player can't over cut
+                            //Update ponderation to 0 for all card > cardPlay
+                            cardPackFiltered = cardPack.stream().filter(
+                                    card -> card.getSuit()==turnRound.getTrumpSuit()
+                                            &&
+                                            card.getCardOrder(turnRound.getTrumpSuit())>cardPlay.getCardOrder(turnRound.getTrumpSuit())
+                            ).collect(Collectors.toList());
+
+                            for(Card card : cardPackFiltered){
+                                updatePonderation(otherPlayer.getPossibleMoves(),0.0,cardPlay);
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+    public static void updatePonderation(HashMap<Card,Double> possibleMoves,Double ponderation, Card card) {
+        possibleMoves.put(card, ponderation);
+    }
+
+
 }
